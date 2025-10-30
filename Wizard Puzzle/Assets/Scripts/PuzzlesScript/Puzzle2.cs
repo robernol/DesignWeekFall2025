6c6784acae28ea34d2250f0761fb6c58ae848ec2
JoeyLam <joeylam020402@gmail.com>
2025-10-30 12:18:12 -0400

    Refactor Puzzle2 UI and update scene objects
    Updated Puzzle2.cs to manage UI elements for puzzle number and instructions, including font asset and visibility logic. Modified Scene1 and Scene6 to adjust navigation hints, font settings, and reorganize hint-related GameObjects for consistency and improved UI handling.

--- Wizard Puzzle/Assets/Scripts/PuzzlesScript/Puzzle2.cs
+++ Wizard Puzzle/Assets/Scripts/PuzzlesScript/Puzzle2.cs
@@ -1,6 +1,29 @@
+using TMPro;
 using UnityEngine;
 
 public class Puzzle2 : MonoBehaviour
 {
-    
+    [Header("Puzzle 2 Text References")]
+    public TMP_FontAsset normalFontAsset;
+    public TextMeshProUGUI puzzleNum;
+    public GameObject instruction;
+
+
+    private void Start()
+    {
+        instruction.SetActive(false);
+
+        if (GlobalVariableManager.solvedPuzzle1)
+        {
+            ShowTranslatedText(puzzleNum, normalFontAsset, puzzleNum.fontSize, 2.ToString());
+            instruction.SetActive(true);
+        }
+    }
+
+    private void ShowTranslatedText(TextMeshProUGUI targetText, TMP_FontAsset targetFont, float targetFontSize, string theOutputText)
+    {
+        targetText.font = targetFont;
+        targetText.fontSize = targetFontSize;
+        targetText.text = theOutputText;
+    }
 }
