7b187d337be4d36a636aaf754b0d5037b34907b0
JoeyLam <joeylam020402@gmail.com>
2025-10-31 10:00:38 -0400

    Remove unused hint logic from FinalPuzzleScript
    Eliminated references to the 'hint' GameObject and related logic in FinalPuzzleScript.cs, as the hint is no longer used. Also removed a debug log statement for cleaner code.

--- Wizard Puzzle/Assets/Scripts/PuzzlesScript/FinalPuzzleScript.cs
+++ Wizard Puzzle/Assets/Scripts/PuzzlesScript/FinalPuzzleScript.cs
@@ -7,15 +7,12 @@ public class FinalPuzzleScript : MonoBehaviour
     public TMP_FontAsset normalFontAsset;
     public TextMeshProUGUI puzzleNum;
 
-    public GameObject hint;
 
     private void Start()
     {
-        hint.SetActive(false);
 
         if (GlobalVariableManager.solvedPuzzle5)
         {
-            hint.SetActive(true);
             ShowTranslatedText(puzzleNum, normalFontAsset, puzzleNum.fontSize, 6.ToString());
         }
     }
@@ -25,7 +22,6 @@ public class FinalPuzzleScript : MonoBehaviour
         if (PlayerInput.W2IsInserted)
         {
             SceneManager.LoadScene(6);
-            Debug.Log("key down");
         }
     }
     private void ShowTranslatedText(TextMeshProUGUI targetText, TMP_FontAsset targetFont, float targetFontSize, string theOutputText)
